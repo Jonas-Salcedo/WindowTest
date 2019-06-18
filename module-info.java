880eb856b455539b9f90ed98cb79fcca8b0c2a66
fudgenuggets12 <jsalcedo1102@gmail.com>
2019-06-17 21:04:08 -0500

    Create module-info.java

---
+++ module-info.java
@@ -0,0 +1,11 @@
+module WindowTest {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires javafx.swing;
+    requires javafx.swt;
+    requires javafx.web;
+    exports WindowTest;
+}
